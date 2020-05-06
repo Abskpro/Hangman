0bbabcd708ea9439cfd589bcd9d421196220dd65
Abhishek <karma1997.acharya@gmail.com>
2020-05-06 17:35:47 +0545

    stop same input

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-window.onload = function() {
-  var ctx = document.getElementById("canvas").getContext("2d");
+(() => {
+  var ctx = document.getElementById('canvas').getContext('2d');
   var words = [
-    "furr",
-    "puff",
-    "greed",
-    "hungry",
-    "puppy",
-    "library",
-    "History",
-    "civilization",
-    "democracy",
-    "public",
-    "meaning",
-    "immune"
+    'furr',
+    'puff',
+    'greed',
+    'hungry',
+    'puppy',
+    'library',
+    'history',
+    'civilization',
+    'democracy',
+    'public',
+    'meaning',
+    'immune',
+    'computer',
+    'world',
+    'potato',
+    'everest',
   ];
+
   var hangman = {
     rope: function() {
       ctx.beginPath();
@@ -60,86 +65,102 @@ window.onload = function() {
       ctx.moveTo(350, 300);
       ctx.lineTo(450, 400);
       ctx.stroke();
-    }
+    },
   };
 
   var parts = [
-    "head",
-    "body",
-    "lefthand",
-    "righthand",
-    "leftleg",
-    "rightleg",
-    "rope"
+    'head',
+    'body',
+    'lefthand',
+    'righthand',
+    'leftleg',
+    'rightleg',
+    'rope',
   ];
 
   let random = Math.floor(Math.random() * 12 + 1);
-  var canvas = document.getElementById("canvas");
+  var canvas = document.getElementById('canvas');
   var selected_word = words[random];
   let spread = [...selected_word];
   console.log(selected_word);
   spread.map(curr => {
-    const li = document.createElement("LI");
-    const guess = document.getElementById("guess");
-    const textnode = document.createTextNode("_");
+    const li = document.createElement('LI');
+    const guess = document.getElementById('guess');
+    const textnode = document.createTextNode('_');
     li.appendChild(textnode);
     li.classList.add(`word_${curr}`);
     guess.appendChild(li);
   });
 
+  var length = 0;
   var counter = 1;
+  var usedWord = [];
   window.onkeypress = function(e) {
-    var count = spread.filter(curr => {
-      return curr === e.key;
-    });
-    var li = document.createElement("LI");
-    var node = document.createTextNode(e.key);
-    li.appendChild(node);
-    document.getElementById("letters-used").appendChild(li);
-    if (count.length != 0) {
-      console.log("it is not empty");
-      count.forEach((curr, index) => {
-        var node = document.getElementsByClassName(`word_${e.key}`)[index];
-        node.innerHTML = e.key;
-      });
-    } else {
-      switch (counter) {
-        case 1:
-          hangman.rope();
-          counter += counter;
-          break;
-        case 2:
-          hangman.head();
-          counter = counter + 1;
-          break;
-        case 3:
-          hangman.body();
-          counter = counter + 1;
-          break;
-        case 4:
-          hangman.lefthand();
-          counter = counter + 1;
-          break;
-        case 5:
-          hangman.righthand();
-          counter = counter + 1;
-          break;
-        case 6:
-          hangman.leftleg();
-          counter = counter + 1;
-          break;
-        case 7:
-          hangman.rightleg();
-          counter = counter + 1;
-          break;
-        default:
-              alert('game over')
+    e.preventDefault();
+    if (!usedWord.includes(e.key)) {
+      if (length != spread.length) {
+        var count = spread.filter(curr => {
+          return curr === e.key;
+        });
+        console.log(count);
+        var li = document.createElement('LI');
+        var node = document.createTextNode(e.key);
+
+        li.appendChild(node);
+        usedWord.push(e.key);
+        document.getElementById('letters-used').appendChild(li);
+        if (count.length != 0) {
+          count.forEach((curr, index) => {
+            length = length + 1;
+            var node = document.getElementsByClassName(`word_${e.key}`)[index];
+            node.innerHTML = e.key;
+          });
+        } else {
+          switch (counter) {
+            case 1:
+              hangman.rope();
+              counter += counter;
+              break;
+            case 2:
+              hangman.head();
+              counter = counter + 1;
+              break;
+            case 3:
+              hangman.body();
+              counter = counter + 1;
+              break;
+            case 4:
+              hangman.lefthand();
+              counter = counter + 1;
+              break;
+            case 5:
+              hangman.righthand();
+              counter = counter + 1;
+              break;
+            case 6:
+              hangman.leftleg();
+              counter = counter + 1;
+              break;
+            case 7:
+              hangman.rightleg();
+              counter = counter + 1;
+              break;
+            default:
+              alert('game over');
               let message = confirm('play again');
-              if(message == true){
-                  location.reload();
+              if (message == true) {
+                location.reload();
               }
-          console.log("gameover");
+              console.log('gameover');
+          }
+        }
+      } else {
+        alert('you win');
+        let message = confirm('play again');
+        if (message == true) {
+          location.reload();
+        }
       }
     }
   };
-};
+})();
